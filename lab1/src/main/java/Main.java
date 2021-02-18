55f0d0573905ff10fc27bb631b6c0df0d7cff706
elizabeth-hira <elizavetagiro@gmail.com>
2021-02-18 18:07:36 +0300

    improved Main

--- lab1/src/main/java/Main.java
+++ lab1/src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        List<String> list = getList(1000).getFirst();
+        List<String> list = getList(100000).getFirst();
         long start, res;
 
         for(int i = 1; i < 11; i++) {
